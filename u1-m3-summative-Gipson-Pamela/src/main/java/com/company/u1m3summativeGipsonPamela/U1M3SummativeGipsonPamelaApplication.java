4f18cf2ced3c937d4653b930c48c1999e273c0e4
pgipson1969 <52027918+pgipson1969@users.noreply.github.com>
2019-07-18 14:22:14 -0500

    initial commit

---
+++ u1-m3-summative-Gipson-Pamela/src/main/java/com/company/u1m3summativeGipsonPamela/U1M3SummativeGipsonPamelaApplication.java
@@ -0,0 +1,13 @@
+package com.company.u1m3summativeGipsonPamela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeGipsonPamelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeGipsonPamelaApplication.class, args);
+	}
+
+}
